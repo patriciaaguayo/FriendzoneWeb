ab41f0a1f965037afe31b58ae6425606c729af09
patriciaaguayo <patriciaaguayop@gmail.com>
2024-05-16 01:07:41 +0200

    Creado documento javascript. Además, se ha modificado la páginas de Secciones y añadido dos botones para unirse a insta y discord

---
+++ js/Friendzone.js
@@ -0,0 +1,19 @@
+"Use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botonesRedesSociales = document.querySelectorAll('.botonRedSocial');
+
+    // Agregar evento de mouseover a cada botón
+    botonesRedesSociales.forEach(boton => {
+        boton.addEventListener('mouseover', () => {
+            boton.style.transform = 'scale(1.4)';
+        });
+    });
+
+    // Agregar evento de mouseout a cada botón
+    botonesRedesSociales.forEach(boton => {
+        boton.addEventListener('mouseout', () => {
+            boton.style.transform = 'scale(1)';
+        });
+    });
+});
